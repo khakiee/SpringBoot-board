fad8ccc9ad162bfe8531d6c16fd52083b61b6a9d
Jihwan Kim <noneobj@gmail.com>
2021-03-24 19:32:36 +0900

    Add HelloResponseDto using lombok

--- src/main/java/com/mySpringProject/main/web/dto/HelloResponseDto.java
+++ src/main/java/com/mySpringProject/main/web/dto/HelloResponseDto.java
@@ -1,2 +1,11 @@
-package com.mySpringProject.main.web.dto;public class HelloResponseDto {
+package com.mySpringProject.main.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
 }
