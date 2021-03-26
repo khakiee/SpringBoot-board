7b54811c8a646866dbb09e885ae03ea11654fb2b
Jihwan Kim <noneobj@gmail.com>
2021-03-26 17:53:00 +0900

    Add domain of posts and its test

---
+++ src/test/java/com/mySpringProject/main/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.mySpringProject.main.web.domain.posts;
+
+import com.mySpringProject.main.domain.posts.Posts;
+import com.mySpringProject.main.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save_post_and_load() {
+        String title = "테스트용 게시글입니다.";
+        String content = "테스트용 본문입니다.";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("kjh@kjh.main")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
